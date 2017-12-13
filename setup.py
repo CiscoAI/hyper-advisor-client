c63007fcac0a8842455e40bea0384af2a155c108
adelliu <lqj679ssn@qq.com>
2017-12-13 14:49:49 -0800

    package

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ setup(
     author='Adel Liu',
     author_email='i@6-79.cn',
     description='auto tuning adviser client',
-    url='https://github.com/ciscoai',
-
+    url='https://github.com/ciscoai/adviser-client',
+    license='MIT',
+    packages={
+        'adviser-client'
+    },
+    entry_points={
+        'console_scripts': ['trail_server = adviser-client.trail_server.py']
+    }
 )
